01969d23fbbeab2258fb6a4110f61090c7c34c38
Milos Seckovic <milossec@gmail.com>
2021-01-11 14:28:13 +0100

    Entity framework, migration added
    - Installed Entity framework
    - Enabled migration

---
+++ HotelManagementSystem.Data/HMSContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagementSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
