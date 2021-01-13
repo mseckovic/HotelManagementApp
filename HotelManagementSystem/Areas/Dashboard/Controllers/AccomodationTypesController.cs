0d3364f623e0ba1888aba5397074a1bf5c9c709c
Milos Seckovic <milossec@gmail.com>
2021-01-13 11:35:01 +0100

    AccomodationType, Create button, DB updated
    - AccomodationType controller and view added
    - Create button added on AccomodationType page
    - Inserted 2 rows in DB for AccomodationType

---
+++ HotelManagementSystem/Areas/Dashboard/Controllers/AccomodationTypesController.cs
@@ -0,0 +1,61 @@
+﻿using HotelManagementSystem.Areas.Dashboard.ViewModels;
+using HotelManagementSystem.Entities;
+using HotelManagementSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementSystem.Areas.Dashboard.Controllers
+{
+    public class AccomodationTypesController : Controller
+    {
+        AccomodationTypesService accomodationTypesService = new AccomodationTypesService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listing()
+        {
+            AccomodationTypesListingModel model = new AccomodationTypesListingModel();
+
+            model.AccomodationTypes = accomodationTypesService.GetAllAccomodationTypes();
+
+            return PartialView("_Listing", model);
+        }
+
+        [HttpGet]
+        public ActionResult Action()
+        {
+            AccomodationTypesActionModel model = new AccomodationTypesActionModel();
+
+            return PartialView("_Action",model);
+        }
+
+        [HttpPost]
+        public JsonResult Action(AccomodationTypesActionModel model)
+        {
+            JsonResult json = new JsonResult();
+
+            AccomodationType accomodationType = new AccomodationType();
+
+            accomodationType.Name = model.Name;
+            accomodationType.Description = model.Description;
+
+            var result = accomodationTypesService.SaveAccomodationType(accomodationType);
+
+            if (result)
+            {
+                json.Data = new { Success = true };
+            }
+            else 
+            {
+                json.Data = new { Success = false, Message = "Unable to add Accomodation Type." };
+            }
+            return json;
+        }
+    }
+}
